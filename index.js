bc275503d2d4504bbc927688432abdd6895c0d56
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-13 13:40:28 -0400

    prompts working and connected to sql

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 })
 
-console.log("wokring");
+console.log("Welcome to B-Amazon!");
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("connect as " + connection.threadId);
-	
+	start();
 })
+
+var start = function (){
+	inquirer.prompt(
+	{
+		name: "welcome",
+		type: "list",
+		message: "Welcome, would you like to see our products? [YES] [NO]",
+		choices: ["YES", "NO"]
+	}).then(function(answer) {
+		if (answer.welcome.toUpperCase() == "YES"){
+			ourProducts();
+		}
+		else {
+			console.log("Goodbye");
+			return;
+		}
+	})
+}
+
+var ourProducts = function(){
+	connection.query('SELECT * FROM products', function(err, res){
+		if (err) throw err;
+		console.log(res);
+	})
+}
