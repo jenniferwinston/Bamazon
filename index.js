20fdd3aa4420cd8cf87ddc8b58f26488fc9e83e2
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-13 17:05:31 -0400

    functions added

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ var connection = mysql.createConnection({
 })
 
 console.log("Welcome to B-Amazon!");
+//connect to sql and start the products
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("connect as " + connection.threadId);
 	start();
 })
 
+// start promopting
 var start = function (){
 	inquirer.prompt(
 	{
 		name: "welcome",
 		type: "list",
-		message: "Welcome, would you like to see our products? [YES] [NO]",
+		message: "Welcome, would you like to shop our products?",
 		choices: ["YES", "NO"]
 	}).then(function(answer) {
 		if (answer.welcome.toUpperCase() == "YES"){
@@ -34,11 +36,67 @@ var start = function (){
 			return;
 		}
 	})
-}
+};
 
-var ourProducts = function(){
-	connection.query('SELECT * FROM products', function(err, res){
-		if (err) throw err;
-		console.log(res);
+
+var ourProducts = function (){
+connection.query('SELECT * FROM products', function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+    	//diplay products offered
+        console.log(res[i].itemID + " | " + res[i].productName + " | " + res[i].departmentName + " | " + "$" + res[i].price);
+    }
+    console.log("-----------------------------------");
+    // asks the next question ater 5 seconds
+    setTimeout(function() {nextAsk();}, 5000);
+})
+};
+
+
+var nextAsk = function (){
+	inquirer.prompt([
+	{
+		name: "productid",
+		type: "list",
+		message: "Choose the ID of the product you wish to purchase:",
+		choices: ["1", "2", "3", "4", "5", "6", "7"]
+	},	
+	{
+		name: "productunits",
+		type: "input",
+		message: "How many units of this product would you like to puchase?"
+		// validate: function(value) {
+		// 	if (isNaN(value) == false) {
+		// 		return true;
+		// 	}
+		// 	else { return false; }
+		// }
+	}]).then(function(answer) {
+			checkQuantity(answer);
 	})
-}
+};
+
+var checkQuantity = function(answer) {
+	var query = 'SELECT stockQuantity, price FROM products WHERE itemID =?';
+	var params = answer.productid;
+
+	connection.query(query, params, function(err, res) {
+		if ( res[0].stockQuantity < answer.productunits) {
+			console.log("Insufficient quantity");
+		}
+		else {
+			var total = answer.productunits * res[0].price;
+			var newQuantity = res[0].stockQuantity-answer.quantity;
+			updateQuantity(answer.productid, total, newQuantity);
+		}
+	})
+
+};
+
+var updateQuantity = function (id, total, newQuantity){
+
+};
+
+
+
+
+
